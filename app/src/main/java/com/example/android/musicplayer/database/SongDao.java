b5f522b80aeb694fcceb8ccc0134982afa19b3ae
Soojeong <soojeongshin2@gmail.com>
2018-05-29 22:31:00 +0900

    Create a DAO

---
+++ app/src/main/java/com/example/android/musicplayer/database/SongDao.java
@@ -0,0 +1,22 @@
+package com.example.android.musicplayer.database;
+
+import android.arch.lifecycle.LiveData;
+import android.arch.persistence.room.Dao;
+import android.arch.persistence.room.Delete;
+import android.arch.persistence.room.Insert;
+import android.arch.persistence.room.Query;
+
+import java.util.List;
+
+@Dao
+public interface SongDao {
+
+    @Query("SELECT * FROM song")
+    LiveData<List<SongEntry>> loadAllSongs();
+
+    @Insert
+    void insertSong(SongEntry songEntry);
+
+    @Delete
+    void deleteSong(SongEntry songEntry);
+}
