e26f89e5d44338bc44023ad22fad100984f4269e
Soojeong <soojeongshin2@gmail.com>
2018-06-03 23:22:00 +0900

    Add playlists to PlaylistsFragment

---
+++ app/src/main/java/com/example/android/musicplayer/adapter/PlaylistAdapter.java
@@ -0,0 +1,97 @@
+package com.example.android.musicplayer.adapter;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.CardView;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import com.example.android.musicplayer.R;
+import com.example.android.musicplayer.Song;
+
+import java.util.List;
+
+public class PlaylistAdapter extends RecyclerView.Adapter<PlaylistAdapter.PlaylistViewHolder> {
+
+    /**
+     * An on-click handler that we've defined to make it easy for an Activity to interface with our RecyclerView
+     */
+    final private ItemClickListener mItemClickListener;
+
+    private Context mContext;
+    private List<Song> mSongs;
+
+    /**
+     * The interface that receives onClick messages.
+     */
+    public interface ItemClickListener {
+        void onItemClickListener(int itemId);
+    }
+
+    /**
+     * Constructor for PlaylistAdapter
+     *
+     * @param context
+     * @param listener Listener for list item clicks
+     */
+    public PlaylistAdapter(Context context, List<Song> songs, ItemClickListener listener) {
+        mContext = context;
+        mSongs = songs;
+        mItemClickListener = listener;
+    }
+
+    @NonNull
+    @Override
+    public PlaylistViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(mContext).inflate(R.layout.playlist_item, parent, false);
+        return new PlaylistViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull PlaylistViewHolder holder, int position) {
+        Song song = mSongs.get(position);
+
+        // Check if an album art is provided for this song or not
+        if(song.hasAlbumArtId()) {
+            // If an album art is available, display the provided album art based on the resource ID
+            holder.albumArtImageView.setImageResource(song.getAlbumArtId());
+        } else {
+            // Otherwise display alternate image
+            holder.albumArtImageView.setImageResource(R.drawable.notes);
+        }
+    }
+
+    @Override
+    public int getItemCount() {
+        if (mSongs == null) {
+            return 0;
+        }
+        return mSongs.size();
+    }
+
+    class PlaylistViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
+        private ImageView albumArtImageView;
+        private CardView cardView;
+
+        public PlaylistViewHolder(View itemView) {
+            super(itemView);
+
+            albumArtImageView = itemView.findViewById(R.id.image);
+            cardView = itemView.findViewById(R.id.cardView);
+            itemView.setOnClickListener(this);
+        }
+
+        /**
+         * Called whenever a user clicks on a item in the list
+         * @param v The View that was clicked
+         */
+        @Override
+        public void onClick(View v) {
+            int position = getAdapterPosition();
+            mItemClickListener.onItemClickListener(position);
+        }
+    }
+}
--- app/src/main/java/com/example/android/musicplayer/fragment/PlaylistsFragment.java
+++ app/src/main/java/com/example/android/musicplayer/fragment/PlaylistsFragment.java
@@ -5,18 +5,26 @@ package com.example.android.musicplayer.fragment;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
-import com.example.android.musicplayer.activity.FavoriteActivity;
 import com.example.android.musicplayer.R;
+import com.example.android.musicplayer.Song;
+import com.example.android.musicplayer.activity.FavoriteActivity;
+import com.example.android.musicplayer.adapter.PlaylistAdapter;
+
+import java.util.ArrayList;
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class PlaylistsFragment extends Fragment {
+public class PlaylistsFragment extends Fragment implements PlaylistAdapter.ItemClickListener{
+
+    private ArrayList<Song> mSongs;
 
     public PlaylistsFragment() {
         // Required empty public constructor
@@ -46,7 +54,42 @@ public class PlaylistsFragment extends Fragment {
                 startActivity(favoriteIntent);
             }
         });
+
+        // Create an list of songs
+        mSongs = new ArrayList<Song>();
+
+        mSongs.add(new Song(getString(R.string.ed_sheeran), R.drawable.ed_sheeran_divide));
+        mSongs.add(new Song(getString(R.string.camila_cabello), R.drawable.camila_cabello_havana));
+        mSongs.add(new Song(getString(R.string.imagine_dragones), R.drawable.imagine_dragones_evolve));
+        mSongs.add(new Song(getString(R.string.taylor_swift), R.drawable.taylor_swift_reputation));
+        mSongs.add(new Song(getString(R.string.maroon_5), R.drawable.maroon5_red_pill_blues));
+        mSongs.add(new Song(getString(R.string.portugal_the_man), R.drawable.portugal_the_man_woodstock));
+        mSongs.add(new Song(getString(R.string.sam_smith), R.drawable.sam_smith_the_thrill_of_it_all));
+        mSongs.add(new Song(getString(R.string.halsey), -1));
+
+        // Get the reference to our RecyclerView from xml. This allows us to do things like set
+        // the adapter of the RecyclerView and toggle the visibility.
+        RecyclerView recyclerView = rootView.findViewById(R.id.rv_playlist);
+
+        LinearLayoutManager layoutManager = new LinearLayoutManager(getContext(), LinearLayoutManager.HORIZONTAL, false);
+
+        // Set LinearLayoutManager which is responsible for measuring and positioning item views within
+        // a RecyclerView into a linear list.
+        recyclerView.setLayoutManager(layoutManager);
+
+        // Use this setting to improve performance if you know that changes in content do not
+        // change the child layout size in the RecyclerView
+        recyclerView.setHasFixedSize(true);
+
+        // The PlaylistAdapter is responsible for displaying each item in the list
+        PlaylistAdapter playlistAdapter = new PlaylistAdapter(getContext(), mSongs, this);
+        recyclerView.setAdapter(playlistAdapter);
+
         return rootView;
     }
 
+    @Override
+    public void onItemClickListener(int itemId) {
+
+    }
 }
