739a278291e43d4f3d963c5c7e03f7afaeefe3c8
Soojeong <soojeongshin2@gmail.com>
2018-06-05 00:12:45 +0900

    Add artist images

--- app/src/main/java/com/example/android/musicplayer/fragment/ArtistsFragment.java
+++ app/src/main/java/com/example/android/musicplayer/fragment/ArtistsFragment.java
@@ -5,7 +5,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
-import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -37,14 +37,14 @@ public class ArtistsFragment extends Fragment implements SongAdapter.ItemClickLi
         // Create an list of songs
         mSongs = new ArrayList<>();
 
-        mSongs.add(new Song(getString(R.string.ed_sheeran), R.drawable.ed_sheeran_divide));
-        mSongs.add(new Song(getString(R.string.camila_cabello), R.drawable.camila_cabello_havana));
-        mSongs.add(new Song(getString(R.string.imagine_dragones), R.drawable.imagine_dragones_evolve));
-        mSongs.add(new Song(getString(R.string.taylor_swift), R.drawable.taylor_swift_reputation));
-        mSongs.add(new Song(getString(R.string.maroon_5), R.drawable.maroon5_red_pill_blues));
-        mSongs.add(new Song(getString(R.string.portugal_the_man), R.drawable.portugal_the_man_woodstock));
-        mSongs.add(new Song(getString(R.string.sam_smith), R.drawable.sam_smith_the_thrill_of_it_all));
-        mSongs.add(new Song(getString(R.string.halsey), -1));
+        mSongs.add(new Song(getString(R.string.ed_sheeran), R.drawable.artist_ed_sheeran));
+        mSongs.add(new Song(getString(R.string.camila_cabello), R.drawable.artist_camila_cabello));
+        mSongs.add(new Song(getString(R.string.imagine_dragones), R.drawable.artist_imagine_dragons));
+        mSongs.add(new Song(getString(R.string.taylor_swift), R.drawable.artist_taylor_swift));
+        mSongs.add(new Song(getString(R.string.maroon_5), R.drawable.artist_maroon5));
+        mSongs.add(new Song(getString(R.string.portugal_the_man), R.drawable.artist_portugal_the_man));
+        mSongs.add(new Song(getString(R.string.sam_smith), R.drawable.artist_sam_smith));
+        mSongs.add(new Song(getString(R.string.halsey), R.drawable.artist_halsey));
 
         // Get the reference to our RecyclerView from xml. This allows us to do things like set
         // the adapter of the RecyclerView and toggle the visibility.
@@ -52,7 +52,7 @@ public class ArtistsFragment extends Fragment implements SongAdapter.ItemClickLi
 
         // Set LinearLayoutManager which is responsible for measuring and positioning item views within
         // a RecyclerView into a linear list.
-        recyclerView.setLayoutManager(new GridLayoutManager(getContext(), 2));
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
 
         // Use this setting to improve performance if you know that changes in content do not
         // change the child layout size in the RecyclerView
